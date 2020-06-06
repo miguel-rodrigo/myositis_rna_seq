d4594a4d1f9ff90a7a50694475774088f90145f0
Mike Desktop PC <miguelrodrigo249@gmail.com>
2020-06-06 19:21:09 +0100

    Changes tested with all.equal and are safely equal

--- 1_differential_expression.R
+++ 1_differential_expression.R
@@ -6,7 +6,7 @@ source("utils_get_differential_expression.R")
 analyze.by.autoantibody <- create.rnaseq.analysis(aggregation.level = "autoantibody")
 analyze.by.disease <- create.rnaseq.analysis(aggregation.level = "disease")
 
-results.by.autoantibody2 <- analyze.by.autoantibody()
-results.by.disease2 <- analyze.by.disease()
+results.by.autoantibody <- analyze.by.autoantibody()
+results.by.disease <- analyze.by.disease()
 
 
