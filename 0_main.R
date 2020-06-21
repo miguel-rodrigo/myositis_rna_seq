94eeb3954f1d0c3b4066e53673af0ea24a5ef323
Mike Desktop PC <miguelrodrigo249@gmail.com>
2020-06-21 21:20:47 +0100

    Rename variable. Read literature miRNAs from new improved source

--- 0_main.R
+++ 0_main.R
@@ -10,21 +10,25 @@ analyze.by.disease <- create.rnaseq.analysis(aggregation.level = "disease")
 results.by.disease <- analyze.by.disease()
 
 table.by.disease <- createResultsTable(results.by.disease)
-onlymirs.disease <- table.by.disease[gene.names %in% mirgenes]
 
-# which are the relevant ones
-# --- union ---
-# which are the literature ones
+mirna.genes <- fread("Datos/all_mir_genes.csv")$mirgenes
+onlymirs.disease <- table.by.disease[gene.names %in% mirna.genes]
+
 set.re.adjusted.pvalues(onlymirs.disease)
 getGenesQualityColumns(onlymirs.disease)
 
 # We take those which are relevant for at least 1 disease
 relevant.mirs <- onlymirs.disease[n_relevant >= 1, gene.names]
-literature.mirs <- fread("Datos/literature_mir_genes.csv")$gene_name
+literature.mirs <- fread("Datos/improved_literature_list.csv")$gene_name
 
 mirs.in.common.disease <- intersect(relevant.mirs, literature.mirs)
 
+
+# which are the relevant ones
+# --- union ---
+# which are the literature ones
 filtered.disease <- onlymirs.disease[gene.names %in% union(relevant.mirs, literature.mirs)]
+
 set.re.adjusted.pvalues(filtered.disease)
 getGenesQualityColumns(filtered.disease)
 
@@ -36,7 +40,9 @@ analyze.by.autoantibody <- create.rnaseq.analysis(aggregation.level = "autoantib
 results.by.autoantibody <- analyze.by.autoantibody()
 
 table.by.aas <- createResultsTable(results.by.autoantibody)
-onlymirs.aas <- table.by.aas[gene.names %in% mirgenes]  # Check where mirrgenes is coming from
+
+mirna.genes <- fread("Datos/all_mir_genes.csv")$mirgenes
+onlymirs.aas <- table.by.aas[gene.names %in% mirna.genes]  # Check where mirrgenes is coming from
 
 # which are the relevant ones
 # --- union ---
@@ -56,4 +62,4 @@ getGenesQualityColumns(filtered.aas)
 
 plot.aas <- create.results.heatmap(filtered.aas)
 
-# TODO: Reshape into markdown to show things more easily (a bit harder but also easy)
+# TODO: Reshape into markdown to show things more easily
